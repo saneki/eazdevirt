1296260ec3b5660cee934edd93e966b73349e324
saneki <s@neki.me>
2015-09-29 19:02:23 -0700

    Have -d/-m options use the same method for printing method stub/devirtualization info

--- src/eazdevirt/Program.Devirtualize.cs
+++ src/eazdevirt/Program.Devirtualize.cs
@@ -1,11 +1,160 @@
 ﻿using System;
+using System.Collections.Generic;
 using System.IO;
+using System.Linq;
 using dnlib.DotNet;
+using dnlib.DotNet.Emit;
 
 namespace eazdevirt
 {
 	public partial class Program
 	{
+		/// <summary>
+		/// Print out information about whether or not a devirtualization attempt was
+		/// successful, and if not why not.
+		/// </summary>
+		/// <param name="options">MonoOptions set from passed command-line arguments</param>
+		/// <param name="module">EazModule</param>
+		/// <param name="attempt">Data about the devirtualization attempt</param>
+		static void PrintAttemptSuccess(MonoOptions options, EazModule module,
+			DevirtualizeAttempt attempt)
+		{
+			var reader = attempt.Reader;
+
+			if (attempt.Successful)
+				Console.WriteLine("--> Devirtualizable");
+			else if (attempt.WasInstructionUnknown)
+			{
+				var matches = module.VirtualInstructions
+							.Where((instr) => { return instr.VirtualCode == reader.LastVirtualOpCode; })
+							.ToArray();
+
+				if (matches.Length > 0)
+				{
+					VirtualOpCode v = matches[0];
+					Console.WriteLine("--> Not yet devirtualizable (contains unknown virtual instruction)");
+					Console.WriteLine("-----> Virtual OpCode = {0} @ [{1}] (0x{2:X8})",
+						reader.LastVirtualOpCode, reader.CurrentInstructionOffset, reader.CurrentVirtualOffset);
+					Console.WriteLine("-----> Delegate method: {0} (MDToken = 0x{1:X8})",
+						v.DelegateMethod.FullName, v.DelegateMethod.MDToken.Raw);
+				}
+				else
+				{
+					Console.WriteLine("--> Not yet devirtualizable (contains unexpected virtual instruction @ [{0}] (0x{1:X8}))",
+						reader.CurrentInstructionOffset, reader.CurrentVirtualOffset);
+				}
+			}
+			else
+				Console.WriteLine("--> Not yet devirtualizable (threw exception)");
+		}
+
+		/// <summary>
+		/// Print out information about a devirtualization attempt.
+		/// </summary>
+		/// <param name="options">MonoOptions set from passed command-line arguments</param>
+		/// <param name="module">EazModule</param>
+		/// <param name="attempt">Data about the devirtualization attempt</param>
+		static void PrintAttempt(MonoOptions options, EazModule module,
+			DevirtualizeAttempt attempt)
+		{
+			var reader = attempt.Reader;
+			var method = attempt.Method;
+			var stub = attempt.VirtualizedMethod;
+			var body = attempt.MethodBody;
+
+			IList<Local> locals = attempt.Successful ?
+				body.Variables : reader.Locals;
+			IList<ExceptionHandler> handlers = attempt.Successful ?
+				body.ExceptionHandlers : reader.ExceptionHandlers;
+			IList<Instruction> instructions = attempt.Successful ?
+				body.Instructions : reader.Instructions;
+
+			// Message prefix
+			String prefix;
+			switch(options.Action)
+			{
+				case ProgramAction.Devirtualize:
+					prefix = "Devirtualized";
+					break;
+				case ProgramAction.Methods:
+				default:
+					prefix = "Found";
+					break;
+			}
+
+			Console.WriteLine("{0} {1} (MDToken = 0x{2:X8})", prefix, method.FullName, method.MDToken.Raw);
+
+			if (options.Action == ProgramAction.Methods || options.Verbose)
+			{
+				Console.WriteLine("--> Position string: {0}", stub.PositionString);
+				Console.WriteLine("--> Position: {0} (0x{0:X8})", stub.Position);
+				Console.WriteLine("--> Resource: {0}", stub.ResourceStringId);
+				Console.WriteLine("--> Crypto key: {0}", stub.ResourceCryptoKey);
+				Console.WriteLine("--> Actual method size: {0} (0x{0:X8})", reader.CodeSize);
+
+				if (options.Action == ProgramAction.Methods)
+					PrintAttemptSuccess(options, module, attempt);
+			}
+
+			if (options.Action == ProgramAction.Methods || options.Verbose)
+			{
+				Console.WriteLine();
+
+				// Print locals
+				if (locals.Count > 0)
+				{
+					Int32 index = 0;
+					Console.WriteLine("Locals:");
+					Console.WriteLine("-------");
+					foreach (var local in locals)
+						Console.WriteLine("local[{0}]: {1}", index++, local.Type.FullName);
+					Console.WriteLine();
+				}
+
+				// Print exception handlers
+				if (handlers.Count > 0)
+				{
+					Int32 index = 0;
+					Console.WriteLine("Exception Handlers:");
+					Console.WriteLine("-------------------");
+					foreach (var handler in handlers)
+					{
+						if (handler.CatchType != null)
+							Console.WriteLine("handler[{0}]: HandlerType = {1}, CatchType = {2}",
+								index++, handler.HandlerType, handler.CatchType);
+						else
+							Console.WriteLine("handler[{0}]: HandlerType = {1}",
+								index++, handler.HandlerType);
+						Console.WriteLine("--> Try:     [{0}, {1}]", handler.TryStart, handler.TryEnd);
+						Console.WriteLine("--> Handler: [{0}, {1}]", handler.HandlerStart, handler.HandlerEnd);
+						Console.WriteLine("--> Filter:  {0}", handler.FilterStart);
+					}
+					Console.WriteLine();
+				}
+
+				// Print instructions
+				if (instructions.Count > 0)
+				{
+					Console.WriteLine("Instructions:");
+					Console.WriteLine("-------------");
+					foreach (var instr in instructions)
+						Console.WriteLine(instr);
+					Console.WriteLine();
+				}
+
+				// Print out exception, if any
+				if (!attempt.Successful && !attempt.WasInstructionUnknown)
+				{
+					Console.Write(attempt.Exception);
+					Console.WriteLine();
+					Console.WriteLine();
+				}
+			}
+
+			if (!(options.Action == ProgramAction.Devirtualize && !options.Verbose))
+				Console.WriteLine();
+		}
+
 		/// <summary>
 		/// Perform "devirtualize" verb.
 		/// </summary>
@@ -25,58 +174,9 @@ namespace eazdevirt
 
 			Devirtualizer devirtualizer = new Devirtualizer(module, opts, logger);
 
-			var results = devirtualizer.Devirtualize((attempt) =>
-			{
+			var results = devirtualizer.Devirtualize((attempt) => {
 				if (attempt.Successful)
-				{
-					var method = attempt.Method;
-					var body = attempt.MethodBody;
-
-					Console.WriteLine("Devirtualized {0} (MDToken = 0x{1:X8})",
-						method.FullName, method.MDToken.Raw);
-
-					if (options.Verbose)
-					{
-						Console.WriteLine();
-
-						// Print locals
-						if (body.HasVariables)
-						{
-							Console.WriteLine("Locals:");
-							Console.WriteLine("-------");
-							foreach (var local in body.Variables)
-								Console.WriteLine("local[{0}]: {1}", local.Index, local.Type.FullName);
-							Console.WriteLine();
-						}
-
-						if (body.HasExceptionHandlers)
-						{
-							Int32 index = 0;
-							Console.WriteLine("Exception Handlers:");
-							Console.WriteLine("-------------------");
-							foreach (var handler in body.ExceptionHandlers)
-							{
-								if (handler.CatchType != null)
-									Console.WriteLine("handler[{0}]: HandlerType = {1}, CatchType = {2}",
-										index++, handler.HandlerType, handler.CatchType);
-								else
-									Console.WriteLine("handler[{0}]: HandlerType = {1}",
-										index++, handler.HandlerType);
-								Console.WriteLine("--> Try:     [{0}, {1}]", handler.TryStart, handler.TryEnd);
-								Console.WriteLine("--> Handler: [{0}, {1}]", handler.HandlerStart, handler.HandlerEnd);
-								Console.WriteLine("--> Filter:  {0}", handler.FilterStart);
-							}
-							Console.WriteLine();
-						}
-
-						// Print instructions
-						Console.WriteLine("Instructions:");
-						Console.WriteLine("-------------");
-						foreach (var instr in body.Instructions)
-							Console.WriteLine(instr);
-						Console.WriteLine();
-					}
-				}
+					PrintAttempt(options, module, attempt);
 			});
 
 			if (results.Empty)
@@ -84,8 +184,7 @@ namespace eazdevirt
 				Console.WriteLine("No virtualized methods found");
 				return;
 			}
-
-			if (results.DevirtualizedCount > 0)
+			else if (!options.Verbose)
 				Console.WriteLine();
 
 			Console.WriteLine("Devirtualized {0}/{1} methods",
--- src/eazdevirt/Program.Methods.cs
+++ src/eazdevirt/Program.Methods.cs
@@ -1,23 +1,10 @@
 ﻿using System;
-using System.Linq;
 using dnlib.DotNet;
-using eazdevirt.IO;
 
 namespace eazdevirt
 {
 	public partial class Program
 	{
-		static void PrintLocals(VirtualizedMethodBodyReader reader)
-		{
-			Console.Write("--> Locals count: {0} [", reader.Info.Locals.Length);
-			for(Int32 i = 0; i < reader.Locals.Count; i++)
-			{
-				if (i != 0) Console.Write(", ");
-				Console.Write(reader.Locals[i].Type);
-			}
-			Console.WriteLine("]");
-		}
-
 		/// <summary>
 		/// Perform "methods" verb.
 		/// </summary>
@@ -32,61 +19,18 @@ namespace eazdevirt
 
 			Devirtualizer devirtualizer = new Devirtualizer(module, logger);
 
-			devirtualizer.Devirtualize((attempt) =>
-			{
-				var method = attempt.VirtualizedMethod;
-				var reader = attempt.Reader;
-
-				Console.WriteLine();
-				Console.WriteLine("{0} (MDToken = 0x{1:X8})", method.Method.FullName, method.Method.MDToken.Raw);
-				Console.WriteLine("--> Position string: {0}", method.PositionString);
-				Console.WriteLine("--> Position: {0} (0x{0:X8})", method.Position);
-				Console.WriteLine("--> Resource: {0}", method.ResourceStringId);
-				Console.WriteLine("--> Crypto key: {0}", method.ResourceCryptoKey);
-				PrintLocals(reader);
-				Console.WriteLine("--> Actual method size: {0} (0x{0:X8})", reader.CodeSize);
-
-				if (attempt.Successful)
-				{
-					Console.WriteLine("--> Devirtualizable");
-					WritePartiallyDevirtualizedMethod(reader);
-				}
-				else if(attempt.WasInstructionUnknown)
-				{
-					var matches = module.VirtualInstructions
-								.Where((instr) => { return instr.VirtualCode == reader.LastVirtualOpCode; })
-								.ToArray();
-
-					if (matches.Length > 0)
-					{
-						VirtualOpCode v = matches[0];
-						Console.WriteLine("--> Not yet devirtualizable (contains unknown virtual instruction)");
-						Console.WriteLine("-----> Virtual OpCode = {0} @ [{1}] (0x{2:X8})",
-							reader.LastVirtualOpCode, reader.CurrentInstructionOffset, reader.CurrentVirtualOffset);
-						Console.WriteLine("-----> Delegate method: {0} (MDToken = 0x{1:X8})",
-							v.DelegateMethod.FullName, v.DelegateMethod.MDToken.Raw);
-
-						if (reader.CurrentInstructionOffset > 0)
-							WritePartiallyDevirtualizedMethod(reader);
-					}
-					else
-					{
-						Console.WriteLine("--> Not yet devirtualizable (contains unexpected virtual instruction @ [{0}] (0x{1:X8}))",
-							reader.CurrentInstructionOffset, reader.CurrentVirtualOffset);
-
-						if (reader.CurrentInstructionOffset > 0)
-							WritePartiallyDevirtualizedMethod(reader);
-					}
-				}
-				else
-				{
-					Console.WriteLine("--> Not yet devirtualizable (threw exception)");
-					WritePartiallyDevirtualizedMethod(reader);
-					Console.Write(attempt.Exception);
-					Console.WriteLine();
-					Console.WriteLine();
-				}
+			var results = devirtualizer.Devirtualize((attempt) => {
+				PrintAttempt(options, module, attempt);
 			});
+
+			if (results.Empty)
+			{
+				Console.WriteLine("No virtualized methods found");
+				return;
+			}
+
+			Console.WriteLine("{0}/{1} method stubs devirtualizable",
+				results.DevirtualizedCount, results.MethodCount);
 		}
 	}
 }
